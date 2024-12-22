a847d1c809aee525fba6ec6bd3ac021755384d6d
Patryk Gajewski <patgaj15@gmail.com>
2024-12-22 23:15:53 +0100

    feat: base arenaContext

---
+++ src/context/ArenaContext.jsx
@@ -0,0 +1,18 @@
+import { createContext, useEffect, useState } from "react";
+
+export const ArenaContext = createContext();
+
+export function ArenaProvider({ children }) {
+  const [inArena, setInArena] = useState(0);
+  const [arenaFull, setArenaFull] = useState(false);
+  useEffect(() => {
+    if (inArena === 2) {
+      setArenaFull(true);
+    } else {
+      setArenaFull(false);
+    }
+  }, [inArena]);
+  console.log(inArena);
+
+  return <ArenaContext.Provider value={{ arenaFull, inArena, setInArena }}>{children}</ArenaContext.Provider>;
+}
