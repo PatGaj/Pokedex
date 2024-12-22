a213b4f6f325d602d6dc30a5814e142e1df1dc63
Patryk Gajewski <patgaj15@gmail.com>
2024-12-22 23:35:06 +0100

    chore: Minor changes like add LoginProvider to main.jsx and change title in index.html

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { DarkModeProvider } from "./context/DarkModeContext.jsx";
+import { LoginProvider } from "./context/LoginContext.jsx";
+import App from "./App.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <DarkModeProvider>
+      <LoginProvider>
+        <App />
+      </LoginProvider>
+    </DarkModeProvider>
+  </StrictMode>
+);
